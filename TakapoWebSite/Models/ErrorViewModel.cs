a84569f0667ec337b5fdb8207b8d4d8122377ac4
vaqef.payfoon@outlook.com <vaqef.payfoon@outlook.com>
2021-09-28 01:06:01 +0330

    Add project files.

---
+++ TakapoWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TakapoWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
